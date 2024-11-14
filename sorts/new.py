6671500ff134887bd2709b8ae2381a463b703c5e
Graham Sawyer <rgrahamsawyer@gmail.com>
2024-11-14 17:45:13 -0500

    udpate in case i want to go back
    in case i permanently break this merge sort

--- sorts/new.py
+++ sorts/new.py
@@ -2,7 +2,6 @@ from random import shuffle
 
 class Sort():
     def __init__(self, unsorted: list, speed: int):
-        self.name = "bubblesort"
         self.sorting = unsorted
         self.speed = speed
         self.iterations = 0
@@ -34,6 +33,7 @@ class BubbleSort(Sort):
     def __init__(self, unsorted:list, speed: int):
         super().__init__(unsorted, speed)
         self.top = len(self.sorting)
+        self.name = "bubblesort"
 
     
     def sort(self):
@@ -56,20 +56,69 @@ class BubbleSort(Sort):
         return [i for i in self.sorting]
     
 
-class MergeSort(Sort):
+class MergeSort(Sort): # make this an in-place method using the left, middle, right thing
     def __init__(self, unsorted:list, speed: int) -> None:
         super().__init__(unsorted, speed)
+        self.sorting = [[i] for i in unsorted]
 
-    def merge(self, left:list, right:list) -> list:
-        
+    def merge(self, left: int, middle: int, right: int) -> None: # there is definitely a better way to do this but this works for now
+        if right is None:
+            return left
+
+        # stuff for keeping self.sorting up to date
+
+
+        output = [i for i in right]
+        i = 0
+        rightElem = right[0]; self.access(rightElem)
+        for leftElem in left:
+            self.access(leftElem)
+            while leftElem > rightElem:
+                i += 1
+                if i >= len(output):
+                    break
+                rightElem = output[i]; self.access(rightElem)
+            output.insert(i, leftElem)
+            i += 1
+            self.increment()
+
+        return(output)
+    
+
+    def mergeList(self, lis: list[list]) -> list:
+        return self.merge(lis[0], lis[1])
+    
+
+    def mergeAll(self) -> list[list]:
+        lis = self.sorting
+        if len(lis) % 2 == 0:
+            partitions = [[lis[i], lis[i+1]] for i in range(0, len(lis), 2)]
+        else:
+            partitions = [[lis[i], lis[i+1]] for i in range(0, len(lis) - 1, 2)] + [[lis[-1], None]]
+
+        for partition in partitions:
+            self.mergeList(partition)
+
+    
+
+    def sort(self) -> list[list]:
+        while len(self.sorting) != 1:
+            self.sorting = self.mergeAll(self.sorting)
+
+
+    def current(self):
+        return [i for j in self.sorting for i in j]
+
     
 
 
 if __name__ == "__main__":
-    l = [i for i in range(50)]
-    shuffle(l)
+    l1 = [i for i in range(11,22,2)]
+    l2 = [i for i in range(2,13,2)]
 
-    a = BubbleSort(l, 50)
+    a = MergeSort([5,2,3,1,4], 1)
+
     a.sort()
-    print(len(a.accessedData))
-    print(len(a.data))
+    print(a.sorting)
+
+    
